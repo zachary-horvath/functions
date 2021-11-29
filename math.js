bd83f5d79cd7249336d874d413040cf4f50d93d9
Zachary Horvath <me@zacharyhorvath.io>
2021-11-29 17:07:56 -0500

    added functions for average and factorial

--- math.js
+++ math.js
@@ -1,15 +1,31 @@
 /*---------- MATH ----------*/
 
+console.log("Let's do some math!");
+console.log("-------------------");
+
 let numbers = [1, 2, 3, 4, 5];
 
 // Compute sum from array of numbers via reduce
 const sum = numbers.reduce(function (result, item) {
   return result + item;
 }, 0);
-console.log(`The sum is: ${sum}.`);
+console.log(`The sum of [${numbers}] is ${sum}.`);
 
 // Compute multiplication from array of numbers via reduce
 const multiply = numbers.reduce(function (result, item) {
   return result * item;
 }, 1);
-console.log(`The product is: ${multiply}.`);
+console.log(`The product of [${numbers}] is ${multiply}.`);
+
+// Compute average from array of numbers via reduce
+const average =
+  numbers.reduce(function (result, item) {
+    return result + item;
+  }, 0) / numbers.length;
+console.log(`The average of [${numbers}] is ${average}.`);
+
+// Compute factorial of n
+const factorial = (n) => {
+  return n === 0 ? 1 : n * factorial(n - 1);
+};
+console.log(`The factorial of 10 is ${factorial(10)}.`);
